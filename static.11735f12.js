fcc4903c7025990921bd80e7468bfeed3f4ff9c5
Cédric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-06-17 14:55:59 +0200

    Updates

--- static.11735f12.js
+++ static.11735f12.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/humantodata/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 11);
@@ -2999,4 +2999,4 @@ exports.default = {
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.c26d583f.js.map
+//# sourceMappingURL=static.11735f12.js.map
